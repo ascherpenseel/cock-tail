c3fe67a84e174c61bb572b70811ba55482eaa5d5
Alejandro <alejandro@crispstudio.es>
2020-09-17 17:36:45 +0200

    ie11

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+import 'proxy-polyfill'
 import '../styles/globals.scss'
 import { AppContextProvider } from '../context/context'
 
